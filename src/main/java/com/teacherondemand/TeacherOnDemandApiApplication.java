f07d7943b0dbf134add42235d6866b6842bad902
José Vitor <josevitor@tce.rn.gov.br>
2026-06-21 19:47:23 -0300

    Init spring project

---
+++ src/main/java/com/teacherondemand/TeacherOnDemandApiApplication.java
@@ -0,0 +1,13 @@
+package com.teacherondemand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherOnDemandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherOnDemandApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teacherondemand/TeacherOnDemandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacherondemand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherOnDemandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
